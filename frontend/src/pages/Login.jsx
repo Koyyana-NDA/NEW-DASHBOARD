1c4a9225a260cf1ff7198767d6481f364d0d003b
PriyaKoyyana <saipriyachem@gmail.com>
2025-07-15 11:38:29 +0530

    Fix jwt-decode import

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 // import { useNavigate } from 'react-router-dom'
-import jwtDecode from "jwt-decode";
+// import jwtDecode from "jwt-decode";
+import { default as jwtDecode } from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 
 
