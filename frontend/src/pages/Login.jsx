78dd21ca078f1dc1a3308cf05ef110756d249ae2
PriyaKoyyana <saipriyachem@gmail.com>
2025-07-15 11:11:19 +0530

    Remove duplicate useNavigate import in Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import jwtDecode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 
