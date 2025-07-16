277e646a8a25e10745d46e5123f08a1ca43bc8a1
PriyaKoyyana <saipriyachem@gmail.com>
2025-07-16 13:13:07 +0530

    added frontend and backend origins

--- backend/config.py
+++ backend/config.py
@@ -61,7 +61,9 @@ APP_CONFIG = {
         "http://localhost:3000",
         "http://localhost:5173",
         "http://127.0.0.1:3000",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://new-dashboard-u2l9.onrender.com",    # ← My front end
+        "https://new-dashboard-backend.onrender.com"  # ← if i need to test from backend’s own origin
     ]
 }
 
